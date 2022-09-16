274ae294d224f127de71cd4cb09884332a178de2
Surya T N <suryatej.nim@yahoo.com>
2022-09-16 17:50:40 +0000

    Added partial UI layout to render until the start button timer control and the timer display

--- src/components/DigitalTimer/index.js
+++ src/components/DigitalTimer/index.js
@@ -73,4 +73,57 @@ export default class DigitalTimer extends Component {
     this.setState(previousTimerState => ({
       timerMinutes: previousTimerState.timerMinutes - 1,
     }))
+
+  render() {
+      const {timerMinutes, timerSeconds, timerIntervalId} = this.state
+
+      const timerMinutesString = timerMinutes < 10 ? '0' + timerMinutes : timerMinutes
+      const timerSecondsString = timerSeconds < 10 ? '0' + timerSeconds : timerSeconds
+
+      return (
+          <div className="digital-timer-bg-container">
+              <h1 className="digital-timer-header">Digital Timer</h1>
+
+              <div className="digital-timer-content-container">
+                  <div className="digital-timer-display-container">
+                    <div className="digital-timer-details-container">
+                        <p className="digital-timer-countdown">{`${timerMinutesString}:${timerSecondsString}`}</p>
+                        <p className="digital-timer-status">{timerIntervalId === null ? 'Paused' : 'Running'}</p>
+                    </div>
+                  </div>
+
+                  <div className="digital-timer-controls-container">
+                      <div className="digital-timer-start-reset-controls-container">
+                          <div className="digital-timer-control">
+                              <button type="button" className="digital-timer-control-button" onClick={timerIntervalId === null ? this.onClickStart : this.onClickPause}>
+                                  <img 
+                                  className="digital-timer-control-img" 
+                                  src={
+                                      timerIntervalId === null ? 
+                                      'https://assets.ccbp.in/frontend/react-js/play-icon-img.png' : 
+                                      'https://assets.ccbp.in/frontend/react-js/pause-icon-img.png'
+                                      } 
+                                  alt={
+                                      timerIntervalId === null 
+                                      ? 'play icon' : 
+                                      'pause icon'
+                                      } 
+
+                                  />
+                              </button>
+
+                              <p className="digital-timer-control-name">
+                                {
+                                    timerIntervalId === null ? 
+                                    'Start' : 
+                                    'Pause'
+                                }
+                             </p>
+                          </div>
+                      </div>
+                  </div>
+              </div>
+          </div>
+      )
+  }
 }
