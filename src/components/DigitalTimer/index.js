b392f5c3105960c109caa45e0b9c8e6cf9512118
Surya T N <suryatej.nim@yahoo.com>
2022-09-16 17:23:00 +0000

    Updated class name: 'digital-time-content-container' to 'digital-timer-content-container' for DigitalTimer class component. Implemented event handlers for timer controls in DigitalTimer class component.

--- src/components/DigitalTimer/index.js
+++ src/components/DigitalTimer/index.js
@@ -1,1 +1,76 @@
-// Write your code here
+import {Component} from 'react'
+import './index.css'
+
+export default class DigitalTimer extends Component {
+  state = {
+    timerMinutes: 25,
+    timerSeconds: 0,
+    timerIntervalId: null,
+  }
+
+  onClickPause = () => {
+    this.setState(previousTimerState => {
+      const {timerIntervalId} = previousTimerState
+      clearInterval(timerIntervalId)
+
+      return {
+        timerIntervalId: null,
+      }
+    })
+  }
+
+  updateTimerCountdown = () => {
+    this.setState(previousTimerState => {
+      let {timerMinutes, timerSeconds, timerIntervalId} = previousTimerState
+
+      if (timerSeconds === 0) {
+        timerMinutes -= 1
+        timerSeconds -= 59
+      } else {
+        timerSeconds -= 1
+      }
+
+      if (timerMinutes === 0) {
+        clearInterval(timerIntervalId)
+        timerIntervalId = null
+      }
+      return {
+        timerMinutes,
+        timerSeconds,
+        timerIntervalId,
+      }
+    })
+  }
+
+  onClickStart = () => {
+    const intervalId = setInterval(this.updateTimerCountdown, 1000)
+
+    this.setState({
+      timerIntervalId: intervalId,
+    })
+  }
+
+  onClickReset = () => {
+    this.setState(previousTimerState => {
+      const {timerIntervalId} = previousTimerState
+
+      clearInterval(timerIntervalId)
+
+      return {
+        timerMinutes: 25,
+        timerSeconds: 0,
+        timerIntervalId: null,
+      }
+    })
+  }
+
+  onTimerIncrement = () =>
+    this.setState(previousTimerState => ({
+      timerMinutes: previousTimerState.timerMinutes + 1,
+    }))
+
+  onTimerDecrement = () =>
+    this.setState(previousTimerState => ({
+      timerMinutes: previousTimerState.timerMinutes - 1,
+    }))
+}
